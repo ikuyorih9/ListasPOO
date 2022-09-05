b105ab234eab5f255576b1b8f198be3b2e26748d
briotisk <86500240+briotisk@users.noreply.github.com>
2022-09-04 22:23:48 -0300

    Add files via upload
    Exercício 3 da lista 1, o qual prevê a criação de um programa que modele polinômios, permitindo adicionar temos, exibir o polinômio na tela e, uma vez fornecido um valor de x, também retorna o valor calculado de P(x).

---
+++ Lista1/Questao3/polinomio.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <set>
+#include <bits/stdc++.h>
+using namespace std;
+
+//classe que representa cada monomio do polinomio
+class termo {
+
+private:
+
+    int grau;
+    float multiplicador;
+
+public:
+
+    void inicia(int p_grau, float p_multiplicador){
+        grau = p_grau;
+        multiplicador = p_multiplicador;
+    }
+
+    int get_grau(){
+        return grau;
+    }
+
+    int get_multiplicador(){
+        return multiplicador;
+    }
+
+    void atualiza_termo(float p_multiplicador){
+        multiplicador += p_multiplicador;
+    }
+
+    void mostra_termo(){
+        cout << multiplicador << "*x^" << grau << " + ";
+    }
+
+};
+
+class polinomio {
+
+private:
+
+    int grau_max;
+    list <termo> *monomios = new list <termo>; 
+    list <termo>::iterator it;
+
+public:
+
+    void inicia(int p_grau) {
+        grau_max = p_grau;
+    }
+
+    void add(int p_grau, float p_multiplicador){
+
+        //verifica se o grau do monomio a ser adicionado e maior que o grau max do poliinomio
+        if(p_grau > grau_max)
+            cout << "ERRO! - o grau do monomio e maior que o grau maximo do polinomio" << endl;
+        
+        else{
+
+            //verifica se o monomio já existe
+            bool flag_existe = false;
+            for(it = monomios->begin(); it != monomios->end(); it++){
+                
+                if(p_grau == it->get_grau()){
+
+                    it->atualiza_termo(p_multiplicador);
+                    flag_existe = true;
+
+                }
+                    
+            }
+        
+            //se nao existe adiciona
+            if(!flag_existe){
+                
+                termo temp;
+                temp.inicia(p_grau, p_multiplicador);
+                monomios->push_front(temp);
+
+            }
+            
+        }        
+        
+    }
+
+    void mostra(){
+
+        cout << "P(x) = ";
+
+        //loop que faz as chamadas do metodo que exibe cada monomio
+        for(it = monomios->begin(); it != monomios->end(); it++){
+            it->mostra_termo();
+        }
+
+        cout << endl;
+
+    }
+
+    float caucula(float x){
+
+        float res = 0;
+
+        //percorre a lista de monomios calculando cada termo com base no valor de x e somando tudo para chegar a resposta final
+        for(it = monomios->begin(); it != monomios->end(); it++){
+            res += pow(x, it->get_grau())*it->get_multiplicador();
+        }
+
+        return res;
+
+    }
+};
+
+int main(void) {
+
+    polinomio P;
+    int p_grau;
+    
+    cout << "Digite o grau max do polinomio: " << endl;
+    cin >> p_grau;
+
+    P.inicia(p_grau);
+
+    int op = 0;
+
+    //loop que apresenta as acoes possiveis 
+    while(op != 4){
+
+        cout << "Digite a operacao desejada: " << endl;
+        cout << "1 - Adicionar Monomio" << endl;
+        cout << "2 - Mostrar Polinomio" << endl;
+        cout << "3 - Calcular" << endl;
+        cout << "4 - Sair" << endl;
+        cin >> op;
+
+        switch (op) {
+
+        case 1:
+
+            int grau_monomio;
+            float multiplicador_monomio;
+
+            cout << "Digite o grau do novo monomio: " << endl;
+            cin >> grau_monomio;
+            cout << "Digite o fator que multiplica o novo monomio: " << endl;
+            cin >> multiplicador_monomio;
+
+            P.add(grau_monomio, multiplicador_monomio);
+
+            break;
+
+        case 2:
+            
+            P.mostra();
+
+            break;
+        
+        case 3:
+        
+            float x;
+            cout << "Digite o valor da variável X:" << endl;
+            cin >> x;
+            cout << "P(" << x << ") = " << P.caucula(x) << endl;
+
+            break;
+        
+        case 4:
+
+            cout << "Ate a proxima = )" << endl;
+            break;
+
+        default:
+            cout << "ERRO! - operacao invalida" << endl;
+        }
+    }
+    
+    return 0; // sucesso
+}
