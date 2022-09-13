c283b1dbd4a184bce75d173ba892c9ae2391d079
ikuyorih9 <106600632+ikuyorih9@users.noreply.github.com>
2022-09-13 11:16:09 -0300

    Esboço de código
    Só implementei a estrutura.

---
+++ primeiraquestao/src/primeiraquestao/Gerente.java
@@ -0,0 +1,12 @@
+package primeiraquestao;
+
+public class Gerente {
+    private String nome;
+    private Cliente clientes[] = new Cliente[10];
+    
+    public void aceitarProduto(Produto produto){
+        //VERIFICA PRODUTO PENDENTE.
+        //
+    }
+    
+}
---
+++ primeiraquestao/src/primeiraquestao/Produto.java
@@ -0,0 +1,10 @@
+
+package primeiraquestao;
+
+public class Produto {
+    private String status;
+    
+    public Produto(){
+        status = "desativado";
+    }
+}
