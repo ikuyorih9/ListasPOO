a0104ac4c0e0f3abfa726a75b539ab2f08a66e7e
ikuyorih9 <106600632+ikuyorih9@users.noreply.github.com>
2022-09-16 09:43:06 -0300

    Atualização do Emprestimo

--- Lista2/Questao1/primeiraquestao/src/primeiraquestao/Emprestimo.java
+++ Lista2/Questao1/primeiraquestao/src/primeiraquestao/Emprestimo.java
@@ -4,4 +4,19 @@ public class Emprestimo extends Produto {
     float valor;
     ContaBancaria conta;
     
+    public float valor;
+    public ContaBancaria conta;
+    
+    public Emprestimo(ContaBancaria conta, float valor){
+        this.valor = valor;
+        this.conta = conta;
+    }
+    
+    public boolean verificaDados(){
+        if(conta.retornarSaldo() >= 0 && valor <= 10*conta.retornarSaldo())
+            return true;
+        else
+            return false;
+    }
+    
 }
