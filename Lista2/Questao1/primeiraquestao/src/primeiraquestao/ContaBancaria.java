08bac6fda63638dfa58147a7c9c6c06feb6c77ff
ikuyorih9 <106600632+ikuyorih9@users.noreply.github.com>
2022-09-16 09:31:15 -0300

    Conta bancaria atualizada
    Ajeitei aas relações

--- Lista2/Questao1/primeiraquestao/src/primeiraquestao/ContaBancaria.java
+++ Lista2/Questao1/primeiraquestao/src/primeiraquestao/ContaBancaria.java
@@ -10,5 +10,36 @@ public class ContaBancaria {
         
     }
     
+    public boolean verificaDados(){
+        saldo = 0;
+        possuiCartao = false;
+        return true;
+    }
+    
+    public void depositar (float valor){
+        saldo += valor;
+        System.out.println("Voce depositou R$" + valor);
+    }
+    
+    public void transferir (float valor){
+        saldo -= valor;
+        System.out.println("Voce transferiu R$" + valor);
+    }
+    
+    public float retornarSaldo(){
+        return saldo;
+    }
+    
+    public void mostraDados(){
+        if(retornarAtivacao()){
+            System.out.print("ATIVADO! e com ");
+            System.out.println("Saldo: R$" + saldo);
+        }
+        else
+            System.out.println("DESATIVADO!");
+        
+        
+    }
+    
     
 }
