f3964b52a4a5d3d9ecd9f36cfce01a11ecb646ae
Gabriel Henrique Brioto <86500240+briotisk@users.noreply.github.com>
2022-09-30 09:05:34 -0300

    Update StackOverflow.java

--- StackOverflow/src/stackoverflow/StackOverflow.java
+++ StackOverflow/src/stackoverflow/StackOverflow.java
@@ -1,11 +1,11 @@
 package stackoverflow;
 /*
     Explicação: Uma vez que os objetos e arrays nunca são colocados na puilha,
-seria difícil gerar um StackOverflow empilhando objetos ou variáveis. Porém, as 
-chamadas de um método são emplhadas pela linguagem na stack. Assim, a estratégia 
-aqui utilizada é forçar o erro empilhando chamadas recursivas de um ´método 
-associado ao meu objeto indiscriminadamente, até que uma excessão do tipo 
-"java.lang.StackOverflowError" seja lançada.
+seria difícil gerar um StackOverflow empilhando variáveis. Porém, as chamadas 
+de um método são emplhadas pela linguagem na stack. Assim, a estratégia aqui 
+utilizada é forçar o erro empilhando várias chamadas recursivas de um método 
+associado ao meu objeto, até que uma excessão do tipo "java.lang.StackOverflowError" 
+seja lançada.
 
 */
 public class StackOverflow {
